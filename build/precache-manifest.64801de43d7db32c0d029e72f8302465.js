b8a73a6dc2271373a55bd40c5cd1c22fa8f7035d
ArturAquino <uchihaartur@yahoo.com.br>
2020-01-12 10:18:08 -0300

    Change gitignore to upload build folder

---
+++ build/precache-manifest.64801de43d7db32c0d029e72f8302465.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d1870bbb1a527f54baf0361e4b115c2",
+    "url": "/my-bookings-test/index.html"
+  },
+  {
+    "revision": "68d6595d7d77f640b8a0",
+    "url": "/my-bookings-test/static/css/main.f9732047.chunk.css"
+  },
+  {
+    "revision": "e605cffa346ad8b786b3",
+    "url": "/my-bookings-test/static/js/2.2c8125c4.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/my-bookings-test/static/js/2.2c8125c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68d6595d7d77f640b8a0",
+    "url": "/my-bookings-test/static/js/main.a87b7b75.chunk.js"
+  },
+  {
+    "revision": "85e019669ffe96b9e277",
+    "url": "/my-bookings-test/static/js/runtime-main.66906594.js"
+  }
+]);
