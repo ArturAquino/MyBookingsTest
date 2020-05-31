5b1425fae899ae4acfd0b0f74b1dbf8549412711
ArturAquino <uchihaartur@yahoo.com.br>
2020-05-31 16:31:06 -0300

    Ajustes de css

--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+// import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 
 
-ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-            <Route path={`${process.env.PUBLIC_URL}/`} exact={true} component={App} />
-        </Switch>
-    </BrowserRouter>, document.getElementById('root'));
+// ReactDOM.render(
+//     <BrowserRouter>
+//         <Switch>
+//             <Route path={`${process.env.PUBLIC_URL}/`} exact={true} component={App} />
+//         </Switch>
+//     </BrowserRouter>, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
